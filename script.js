fea0d9070c1f1c95a9e75210ee921aab22f15eee
theAman30 <a.srivastav1005@gmail.com>
2024-02-25 22:56:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function age() {
+  var d1 = document.getElementById("date").value.trim();
+  var m1 = document.getElementById("month").value.trim();
+  var y1 = document.getElementById("year").value.trim();
+
+  if (d1 === "" || m1 === "" || y1 === "") {
+    document.getElementById("age").innerHTML = "Please enter your complete date of birth.";
+    return;
+  }
+
+  var date = new Date();
+  var d2 = date.getDate();
+  var m2 = 1 + date.getMonth();
+  var y2 = date.getFullYear();
+  var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  if (d1 > d2) {
+    d2 = d2 + month[m2 - 1];
+    m2 = m2 - 1;
+  }
+
+  if (m1 > m2) {
+    m2 = m2 + 12;
+    y2 = y2 - 1;
+  }
+
+  var d = d2 - d1;
+  var m = m2 - m1;
+  var y = y2 - y1;
+
+  document.getElementById("age").innerHTML = "You are " + y + "years " + m + "months and " + d + "days old.";
+}
